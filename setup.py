20096a5888bc5ede824708df12620fc44ee19ff4
Johan Rydberg <johan.rydberg@gmail.com>
2011-09-20 18:28:11 +0200

    bump version and install scripts

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(name='fechter',
+      version='0.0.1',
+      description='a simple high-availability manager',
+      author='Johan Rydberg',
+      author_email='johan.rydberg@gmail.com',
+      url='http://github.com/jrydberg/fechter',
+      packages=find_packages() + ['twisted.plugins'],
+      scripts=['bin/fechter']
+)
