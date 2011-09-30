e0ececd1ffe9f4e5236a404cca1c6bb76eb4bbfc
Johan Rydberg <johan.rydberg@gmail.com>
2011-09-30 11:50:08 +0200

    bump version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(name='fechter',
-      version='0.0.3',
+      version='0.0.4',
       description='a simple high-availability manager',
       author='Johan Rydberg',
       author_email='johan.rydberg@gmail.com',
